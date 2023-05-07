c374a24f8c1c37457c2bacb7d36d0110210e37d2
Eyetu Kingsley <eyetukingsley330@gmail.com>
2023-05-07 01:14:20 +0100

    I have completed the first dashboard page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,18 @@ module.exports = {
         'blue1': '#273658',
         'blue2': '#182237',
         'blue3': '#9BACD3',
+        'dash-blue': '#F5FFF9',
+        'mild-black': '#484747',
+        'brand-black': '#222222'
       },
       fontFamily: {
        poppins: [ "Poppins", "sans-serif" ]
+      },
+      zIndex: {
+        '60': '60'
+      },
+      fontSize: {
+        15: '15px'
       }
     },
   },
